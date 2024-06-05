1ba94e401cdafce184fa3429ed67c671c4e73583
Juawir <ytbreven@gmail.com>
2024-06-05 15:26:34 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore quod sunt. Modi unde, officia necessitatibus in fuga facere eos perferendis assumenda molestias delectus rem maxime velit nihil voluptatibus esse!</p>
 </body>
 </html>
